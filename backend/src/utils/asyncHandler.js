b3fff7c0016d04e88380179ad20749b86ea9798e
ankit saini <sainiankit21981@gmail.com>
2025-10-09 19:57:15 +0530

    feat: add ApiError, ApiResponse, and asyncHandler utility classes for improved error handling and response management

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+  try {
+    return await requestHandler(req, res, next);
+  } catch (error) {
+    const statusCode =
+      error.statusCode && error.statusCode >= 100 && error.statusCode < 1000
+        ? error.statusCode
+        : 500;
+    res.status(statusCode).json({
+      success: false,
+      message: err.message || "Something went wrong",
+    });
+  }
+};
+
+export { asyncHandler };
