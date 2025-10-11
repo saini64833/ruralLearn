1c36e2ac0b1a6c066b302092db6c02095999a087
ankit saini <sainiankit21981@gmail.com>
2025-10-11 17:36:01 +0530

    feat: add logout functionality and JWT verification middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -101,4 +101,23 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+const logOutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  return res
+    .status(200)
+    .clearCookie("accessToken", option)
+    .clearCookie("refreshToken", option)
+    .json(new ApiResponse(200, {}, "User successfully loged out"));
+});
+
+export { registerUser, loginUser, logOutUser };
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt, { verify } from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+const verifyJwt = asyncHandler(async (req, _, next) => {
+  try {
+    const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer", "");
+    if (!token) {
+      throw new ApiError(401, "unAuthorized request");
+    }
+
+    const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+    if (!user) {
+      throw new ApiError(401, "user does not exist!!");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(400, error?.message || "invailid accessToken!!");
+  }
+});
