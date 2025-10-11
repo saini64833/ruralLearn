affe8f44be6a063435b6966801cda2cb42c8b54f
ankit saini <sainiankit21981@gmail.com>
2025-10-11 16:32:06 +0530

    feat: implement user registration and login functionality with token generation and error handling

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,104 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (req, res) => {});
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    console.log(error.message);
+    throw new ApiError(500, "something went wrong!!");
+  }
+};
 
-export { registerUser };
+const option = {
+  httpOnly: true,
+  secure: true,
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { password, fullName, userName, email, role, grade, school } = req.body;
+  console.log("email:", email);
+  console.log("password:", password);
+  console.log("fullName:", fullName);
+  console.log("userName:", userName);
+  if (
+    [fullName, userName, email, password, role, grade, school].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All Field Are Required!");
+  }
+  const existedUser = await User.findOne({
+    $or: [{ email }, { userName }],
+  });
+  if (existedUser) {
+    throw new ApiError(400, "User allready exist!!");
+  }
+  const avatarLocalPath = await req.files?.avatar?.[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avtar local path required!!");
+  }
+  const user = await User.create({
+    password,
+    fullName,
+    userName: userName.toLowerCase(),
+    email,
+    role,
+    grade,
+    school,
+    avatar: avatar.url,
+  });
+  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong registering user");
+  }
+  return res.status(201).json(new ApiResponse(200, createdUser, "user registered successfully"));
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { userName, email, password, role } = req.body;
+  if (!(userName || email || role)) {
+    throw new ApiError(400, "email,role and username equired");
+  }
+  const user = await User.findOne({
+    $or: [{ email }, { role }, { userName }],
+  });
+  if (!user) {
+    throw new ApiError(400, "User does not exist!!");
+  }
+
+  const isPasswordVailid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordVailid) {
+    throw new ApiError(401, "password is invailid!!");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+
+  const logedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+  console.log(logedInUser);
+
+  return res
+    .status(200)
+    .cookie("refreshToken", refreshToken, option)
+    .cookie("accessToken", accessToken, option)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: logedInUser,
+          refreshToken,
+          accessToken,
+        },
+        "User logged in successfully!!"
+      )
+    );
+});
+
+export { registerUser, loginUser };
