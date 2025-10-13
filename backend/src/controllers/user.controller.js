7383ec9aec61e1e25e6ca1d24cc1b35b6036b73e
ankit saini <sainiankit21981@gmail.com>
2025-10-13 11:30:53 +0530

    feat: implement change password functionality and update user routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -158,6 +158,23 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(400, "user does not exist");
+  }
+  const oldPasswordIsVailid = await user.isPasswordCorrect(oldPassword);
+  if (!oldPasswordIsVailid) {
+    throw new ApiError(401, "old password is invailid");
+  }
+  user.password=newPassword
+  await user.save({validateBeforeSave:false})
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,{},"password is successfully changed")
+  )
+});
 
-
-export { registerUser, loginUser, logOutUser, refreshAccessToken };
+export { registerUser, loginUser, logOutUser, refreshAccessToken,changeCurrentPassword};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { upload } from "../middlewares/multer.middleware.js";
 
-import { loginUser, logOutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logOutUser, registerUser,refreshAccessToken,changeCurrentPassword} from "../controllers/user.controller.js";
 
 import {verifyJwt} from "../middlewares/auth.middleware.js"
 
@@ -21,4 +21,6 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJwt,logOutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJwt,changeCurrentPassword)
 export default router;
