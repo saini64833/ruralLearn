7bc5e4a402e7310bc09e8f6b8687c658813e8d86
ankit saini <sainiankit21981@gmail.com>
2025-10-14 11:27:04 +0530

    feat: update frontend configuration and remove unused files

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import jwt, { verify } from "jsonwebtoken";
+import jwt  from  "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/apiError.js";
