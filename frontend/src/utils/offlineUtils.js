c2775178fff7a756cac82f28a5542bfca1c73205
ankit saini <sainiankit21981@gmail.com>
2026-01-26 01:40:52 +0530

    refactor: remove unused service worker and utility functions for offline support

--- frontend/src/utils/offlineUtils.js
+++
@@ -1,9 +0,0 @@
-export const isOnline = () => navigator.onLine;
-
-export const onOffline = (callback) => {
-  window.addEventListener("offline", callback);
-};
-
-export const onOnline = (callback) => {
-  window.addEventListener("online", callback);
-};
