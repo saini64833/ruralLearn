f09bd0d279d578803bc65cff5ba41791e45ea3d2
ankit saini <sainiankit21981@gmail.com>
2026-02-01 16:21:47 +0530

    refactor: update VitePWA configuration by removing unused assets and correcting offline fallback URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,11 +10,18 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.ico",
-        "robots.txt",
-        "icons/icon-192.png",
-        "icons/icon-512.png",
+      includeAssets: ["icons/icon-192.png", "icons/icon-512.png"],
+      icons: [
+        {
+          src: "/icons/icon-192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/icons/icon-512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
       ],
       manifest: {
         name: "Rural Learn",
@@ -30,7 +37,7 @@ export default defineConfig({
       },
       workbox: {
         cleanupOutdatedCaches: true,
-        navigateFallback: "/offline", // redirect to offline page if page fails
+        navigateFallback: "/index.html", // redirect to offline page if page fails
         runtimeCaching: [
           {
             urlPattern: ({ request }) => request.mode === "navigate",
