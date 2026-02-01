905315db9bf43660450d36c56c1255077950e60d
ankit saini <sainiankit21981@gmail.com>
2026-02-01 16:34:40 +0530

    fix: update API endpoints in lessonService and quizService for fetching all lessons and quizzes

--- frontend/src/services/lessonService.js
+++ frontend/src/services/lessonService.js
@@ -5,7 +5,7 @@ export const getLessons = async () => {
   const db = await dbPromise;
 
   if (navigator.onLine) {
-    const res = await axios.get("/lessons");
+    const res = await axios.get("/lessons/get-all-lessons");
 
     res.data.forEach((l) => db.put("lessons", l));
 
--- frontend/src/services/quizService.js
+++ frontend/src/services/quizService.js
@@ -1,8 +1,11 @@
+import axios from "../api/axiosInstance";
+import { dbPromise } from "../db/indexedDb";
+
 export const getQuizzes = async () => {
   const db = await dbPromise;
 
   if (navigator.onLine) {
-    const res = await axios.get("/quize");
+    const res = await axios.get("/quizzes/get-all-quizzes");
     res.data.forEach((quiz) => {
       db.put("quizzes", quiz);
     });
